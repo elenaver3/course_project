60251b5ac53f7637e937f3228ac2d34a393abe9c
ElenaVerstova <laniaet@gmail.com>
2023-07-03 03:08:46 +0300

    verstion 1.01

--- restaurant/src/Main.java
+++ restaurant/src/Main.java
@@ -1,5 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        /*
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://std-mysql.ist.mospolytech.ru:3306/std_2277_restaurant",
+                    "std_2277_restaurant", "restaurant2052");
+
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM measurementUnits";
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String unit = result.getString("unit");
+
+                System.out.println(id +  " " + unit);
+
+            }
+
+            connection.close();
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }*
+
+         */
     }
 }
